888884bbf7c5ca7341fb10005ac5177829b43e5c
Tigran Mesropyan <tikodeveloper@gmail.com>
2021-04-07 23:32:55 +0400

    DefinitionCompiler with Unit Tests.

--- tests/Orm/Definition/EntityDefinitionProviderTest.php
+++ tests/Orm/Definition/EntityDefinitionProviderTest.php
@@ -5,9 +5,12 @@ declare(strict_types = 1);
 namespace App\Tests\Orm\Definition;
 
 use App\Orm\Definition\DefinitionCompiler;
+use App\Orm\Definition\EntityDefinition;
 use App\Orm\Definition\EntityDefinitionLoader;
 use App\Orm\Definition\EntityDefinitionProvider;
 use App\Orm\Definition\Exception\DefinitionNotFoundException;
+use Psr\Cache\CacheItemInterface;
+use Psr\Cache\CacheItemPoolInterface;
 use Symfony\Component\Cache\Adapter\PhpFilesAdapter;
 use Symfony\Component\Finder\Finder;
 
@@ -55,12 +58,43 @@ class EntityDefinitionProviderTest extends DefinitionAssertions
         $provider->fetchEntityDefinition('spaceship');
     }
 
-    protected function createProviderInstance() : EntityDefinitionProvider
+    public function testFetchEntityDefinition_FirstLooksInCacheForDefinition() : void
+    {
+        $cache = $this->createMock(PhpFilesAdapter::class);
+        $item = $this->createMock(CacheItemInterface::class);
+
+        // Set up the expectation for the isHit() method to be called only once
+        $item
+            ->expects($this->once())
+            ->method('isHit')
+            ->willReturn(true);
+
+        // Set up the expectation for the get() method to be called only once
+        $item
+            ->expects($this->once())
+            ->method('get')
+            ->willReturn(new EntityDefinition('button', true, false, false, []));
+
+        // Set up the expectation for the getItem() method
+        // to be called only once and with the string 'button' as its parameter.
+        $cache
+            ->expects($this->once())
+            ->method('getItem')
+            ->with($this->equalTo('button'))
+            ->willReturn($item);
+
+        // Create the EntityDefinitionProvider instance and attach the mocked
+        // PhpFilesAdapter object to it.
+        $provider = $this->createProviderInstance($cache);
+        $provider->fetchEntityDefinition('button');
+    }
+
+    protected function createProviderInstance(CacheItemPoolInterface $pool = null) : EntityDefinitionProvider
     {
         return new EntityDefinitionProvider(
             __DIR__.'/definitions',
             new EntityDefinitionLoader(new Finder(), new DefinitionCompiler()),
-            new PhpFilesAdapter('definitions', 0, __DIR__.'/cache')
+            $pool ?? new PhpFilesAdapter('definitions')
         );
     }
 }
